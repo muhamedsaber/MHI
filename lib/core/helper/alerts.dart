8dd91932dcd18edfec363b69bd1d58a08b32207b
saber son <elmohandesstamera@gmail.com>
2024-06-06 12:38:30 +0300

    Refactor Alerts class to make it easy to access alerts throught the class instanse name

--- lib/core/helper/alerts.dart
+++ lib/core/helper/alerts.dart
@@ -2,34 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:mhi/core/helper/app_colors.dart';
 
-void showCustomToast({
-  required String message,
-  required Color color,
-}) {
-  Fluttertoast.showToast(
-    msg: message,
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 1,
-    backgroundColor: color,
-    textColor: Colors.white,
-    fontSize: 16.0,
-  );
-}
-
+class Alerts {
+  void showCustomToast({
+    required String message,
+    required Color color,
+  }) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: color,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
 
 // loading indicator
-customLoadingindicator(BuildContext context) {
-  return showAdaptiveDialog(
-    barrierDismissible: false,
-    context: context,
-    builder: (context) {
-      return const Center(
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(AppColors.lightGreen),
-        ),
-      );
-    },
-  );
+  customLoadingindicator(BuildContext context) {
+    return showAdaptiveDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.lightGreen),
+          ),
+        );
+      },
+    );
+  }
 }
-
