7d91fa3493a9942a9575c099920d088d13c41a2f
saber son <elmohandesstamera@gmail.com>
2024-06-06 18:55:17 +0300

    Update : search textfield for better ui

--- lib/core/common_ui/widgets/search_textfield.dart
+++ lib/core/common_ui/widgets/search_textfield.dart
@@ -1,68 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mhi/core/helper/app_colors.dart';
+import 'package:mhi/core/helper/app_padding.dart';
+import 'package:mhi/core/helper/app_textstyles.dart';
 
-class SearchTextField extends StatefulWidget {
+class SearchTextField extends StatelessWidget {
   const SearchTextField(
       {super.key,
-      required this.onSubmitted,
       required this.hintText,
-      required this.isFocused,
-      required this.isItArabic});
-  final void Function(String)? onSubmitted;
-  final String hintText;
-  final bool isFocused;
-  final bool isItArabic;
-  @override
-  State<SearchTextField> createState() => _SearchTextFieldState();
-}
+      required this.onSubmitted,
+      required this.onButtonPressed,
+      required this.onChanged});
 
-class _SearchTextFieldState extends State<SearchTextField> {
+  final void Function(String)? onSubmitted;
+  final void Function()? onButtonPressed;
+  final String hintText;
+  final void Function(String)? onChanged;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Theme.of(context).cardColor,
-      ),
-      child: TextField(
-        autofocus: widget.isFocused,
-        onSubmitted: widget.onSubmitted,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.onPrimary,
-          decorationThickness: 0,
-          fontWeight: FontWeight.w600,
-          fontSize: 25,
-          letterSpacing: 1.2,
-        ),
-        textDirection:
-            widget.isItArabic ? TextDirection.rtl : TextDirection.ltr,
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          hintTextDirection: TextDirection.ltr,
-          hintStyle: TextStyle(
-              fontSize: 20, color: Theme.of(context).colorScheme.onPrimary),
-          suffixIcon: Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search,
-                  color: Theme.of(context).colorScheme.onPrimary,
-                )),
+    return Padding(
+      padding: AppPadding.symmetricHorizontalMedium,
+      child: SizedBox(
+        height:50.h,
+        child: TextField(
+          onSubmitted: onSubmitted,
+          textAlign: TextAlign.end,
+          keyboardType: TextInputType.text,
+          textDirection: TextDirection.ltr,
+          onChanged: onChanged,
+          style: AppTextStyles.jannat20BoldOnPrimaryColor(context),
+          decoration: InputDecoration(
+            isDense: true,
+            hintText: hintText,
+            hintStyle: AppTextStyles.jannat20BoldOnPrimaryColor(context).copyWith(
+              color: Colors.grey,
+              fontSize: 18.sp,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.w,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(
+                color: AppColors.lighBlue,
+                width: 2.w,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(
+                color: AppColors.lighBlue,
+                width: 2.w,
+              ),
+            ),
           ),
-          border: _borderBuilder(),
-          enabledBorder: _borderBuilder(),
-          focusedBorder: _borderBuilder(),
         ),
-      ),
-    );
-  }
-
-  OutlineInputBorder _borderBuilder() {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(15),
-      borderSide: BorderSide(
-        width: 2,
-        color: Theme.of(context).cardColor,
       ),
     );
   }
