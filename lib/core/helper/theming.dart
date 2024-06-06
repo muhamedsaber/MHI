49ff47b8715dc1dff0511dc9a4490fc1285b5b05
saber son <elmohandesstamera@gmail.com>
2024-06-06 16:58:48 +0300

    feat: Add AppThemeing class for custom theming ui related themes

---
+++ lib/core/helper/theming.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mhi/config/Theme/theme_colors.dart';
+
+class AppThemeing {
+  static LinearGradient patientGradient = const LinearGradient(
+    colors: [LightThemeColors.lightGreen, LightThemeColors.deepBlue],
+  );
+}
