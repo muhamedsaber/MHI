20c5f7132c49dace98aebf3adcb8b4b0ba1be100
saber son <elmohandesstamera@gmail.com>
2024-06-09 09:24:23 +0300

    shimmer widget for doctors list result

---
+++ lib/features/patient/search/search_for_doctors.dart/presentation/widgets/doctors_shimmer_listviewbuilder.dart
@@ -0,0 +1,116 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mhi/core/helper/app_colors.dart';
+import 'package:mhi/core/helper/app_padding.dart';
+import 'package:mhi/core/helper/extensions.dart';
+import 'package:mhi/core/helper/spacing.dart';
+import 'package:shimmer/shimmer.dart';
+
+
+class DoctorShimmerListViewBuilder extends StatelessWidget {
+  const DoctorShimmerListViewBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount:10,
+        itemBuilder: (context, index) {
+       return DoctorBookCardShimmer();
+      },),
+    );
+  }
+}
+
+class DoctorBookCardShimmer extends StatelessWidget {
+  const DoctorBookCardShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120.h,
+      width: context.width,
+      child: Card(
+        color: context.theme.colorScheme.onPrimary.withOpacity(0.1),
+        margin: AppPadding.symmetricHorizontalVerticalMedium,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Shimmer.fromColors(
+                        baseColor: context.theme.scaffoldBackgroundColor,
+                        highlightColor: AppColors.lighGrey.withOpacity(0.3),
+                        child: Container(
+                          width: 100.w,
+                          height: 20.h,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  horizontalSpace(10),
+                  Shimmer.fromColors(
+                       baseColor: context.theme.scaffoldBackgroundColor,
+                        highlightColor: AppColors.lighGrey.withOpacity(0.3),
+                    child: Container(
+                      width: 40.w,
+                      height: 40.h,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              verticleSpace(5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Shimmer.fromColors(
+                             baseColor: context.theme.scaffoldBackgroundColor,
+                        highlightColor: AppColors.lighGrey.withOpacity(0.3),
+                        child: Container(
+                          width: 100.w,
+                          height: 20.h,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  horizontalSpace(10),
+                  Shimmer.fromColors(
+                      baseColor: context.theme.scaffoldBackgroundColor,
+                        highlightColor: AppColors.lighGrey.withOpacity(0.3),
+                    child: Container(
+                      width: 100.w,
+                      height: 20.h,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
