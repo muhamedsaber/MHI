05735ef46caf4a20a3a8f3a14f919b3f3f625628
saber son <elmohandesstamera@gmail.com>
2024-06-09 11:25:25 +0300

    Refactor doctor book card list view builder widget

--- lib/features/patient/search/search_for_doctors.dart/presentation/widgets/doctor_book_card_list_view_builder.dart
+++ lib/features/patient/search/search_for_doctors.dart/presentation/widgets/doctor_book_card_list_view_builder.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
 import 'package:mhi/features/patient/search/search_for_doctors.dart/data/doctors/models/book_doctors_model.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/widgets/doctor_book_card.dart';
+import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/widgets/doctor_book_profile.dart';
 
 class DoctorBookCardListViewBuilder extends StatelessWidget {
-  const DoctorBookCardListViewBuilder({super.key,
-  required this.model
-  });
-final BookDoctorsModel model;
+  const DoctorBookCardListViewBuilder({super.key, required this.model});
+  final BookDoctorsModel model;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -14,7 +14,20 @@ final BookDoctorsModel model;
         physics: const BouncingScrollPhysics(),
         itemCount: model.doctors.length,
         itemBuilder: (context, index) {
-          return  DoctorBookCard(model: model.doctors[index],);
+          return GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    builder: (context) {
+                      return DoctorBookingProfile(
+                        model: model.doctors[index],
+                      );
+                    });
+              },
+              child: DoctorBookCard(
+                model: model.doctors[index],
+              ));
         },
       ),
     );
