24a835af391302270d310de95e27e7c73d024830
saber son <elmohandesstamera@gmail.com>
2024-06-09 11:25:31 +0300

    Add NoDataFound widget for displaying empty API data

--- lib/features/patient/search/search_for_doctors.dart/presentation/widgets/doctors_book_card_bloc_builder.dart
+++ lib/features/patient/search/search_for_doctors.dart/presentation/widgets/doctors_book_card_bloc_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mhi/core/common_ui/widgets/no_data_found.dart';
 import 'package:mhi/core/helper/app_textstyles.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/logic/doctors/book_doctors_cubit.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/widgets/doctor_book_card_list_view_builder.dart';
@@ -19,6 +20,9 @@ class DoctorsBookCardBlocBuilder extends StatelessWidget {
           return const DoctorShimmerListViewBuilder();
         }
         if (state is BookDoctorsLoaded) {
+          if (state.doctors.doctors.isEmpty) {
+            return const NoDataFound(message:"لم يتم العثور علي أطباء",);
+          }
           return  DoctorBookCardListViewBuilder(model:state.doctors,);
         } else if (state is BookDoctorsError) {
           return Center(
