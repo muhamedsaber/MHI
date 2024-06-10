ef4730dbf0f47f70ea33a2d3f9d5de33fbc4220e
saber son <elmohandesstamera@gmail.com>
2024-06-10 08:30:12 +0300

    chore: Update PatientDatabase class to use async/await for saving and deleting doctors

--- lib/config/database/local/patient/patient_database.dart
+++ lib/config/database/local/patient/patient_database.dart
@@ -1,40 +1,50 @@
 import 'dart:convert';
 
-import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 import 'package:mhi/core/constants/database_constants.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/data/doctors/models/book_doctors_model.dart';
 
 class PatientDatabase {
   PatientDatabase._();
-  static saveDoctor(BookModel model) {
+  static Future<bool> saveDoctor(BookModel model) async {
     try {
       String strModel = jsonEncode(model.toJson());
 
       var box = Hive.box(DatabaseConstants.patientDoctorsListBoxKey);
-      box.put(model.doctorId, strModel);
+      await box.put(model.doctorId, strModel);
+      return true;
     } catch (e) {
-      debugPrint(e.toString());
+      return false;
     }
   }
 
-  static deleteDoctor(BookModel model) {
+  static Future<bool>deleteDoctor(BookModel model)async {
     try {
       var box = Hive.box(DatabaseConstants.patientDoctorsListBoxKey);
-      box.delete(model.doctorId);
+    await  box.delete(model.doctorId);
+      return true;
     } catch (e) {
-      debugPrint(e.toString());
+      return false;
     }
   }
 
-  static Future<bool> isDoctorSaved(BookModel model) async{
+  static Future<bool> isDoctorSaved(BookModel model) async {
     var box = Hive.box(DatabaseConstants.patientDoctorsListBoxKey);
     final Iterable<dynamic> list = box.values;
 
     List<BookModel> books =
         list.map((e) => BookModel.fromJson(jsonDecode(e))).toList();
-    bool isSaved =
-        books.where((e) => e.doctorId == model.doctorId).isNotEmpty;
-        return isSaved;
+    bool isSaved = books.where((e) => e.doctorId == model.doctorId).isNotEmpty;
+    return isSaved;
+  }
+
+  static Future<List<BookModel>> getSavedDoctors() async {
+    await Hive.openBox(DatabaseConstants.patientDoctorsListBoxKey);
+    var box = Hive.box(DatabaseConstants.patientDoctorsListBoxKey);
+    final Iterable<dynamic> list = box.values;
+
+    List<BookModel> books =
+        list.map((e) => BookModel.fromJson(jsonDecode(e))).toList();
+    return books;
   }
 }
