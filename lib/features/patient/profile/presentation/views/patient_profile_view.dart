3428442b70487f45fbafbc912d2d91cdbea71b5c
saber son <elmohandesstamera@gmail.com>
2024-06-10 09:20:30 +0300

    Fixing Bug: Report is Exist as a comment in the file

--- lib/features/patient/profile/presentation/views/patient_profile_view.dart
+++ lib/features/patient/profile/presentation/views/patient_profile_view.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mhi/config/database/cache/user_cache.dart';
-import 'package:mhi/core/common_ui/widgets/data_wide_shape.dart';
-import 'package:mhi/core/common_ui/widgets/main_app_bar.dart';
-import 'package:mhi/core/common_ui/widgets/no_data_found.dart';
+import 'package:mhi/config/router/routes.dart';
 import 'package:mhi/core/common_ui/widgets/options_button.dart';
-import 'package:mhi/core/common_ui/widgets/profile_card.dart';
-import 'package:mhi/core/di/dependency_injection.dart';
-import 'package:mhi/core/helper/app_colors.dart';
-import 'package:mhi/core/helper/app_textstyles.dart';
 import 'package:mhi/core/helper/extensions.dart';
 import 'package:mhi/core/helper/spacing.dart';
-import 'package:mhi/core/helper/theming.dart';
-import 'package:mhi/features/auth/login/data/models/patient_model.dart';
+import 'package:mhi/features/patient/profile/presentation/views/patient_settings_view.dart';
 import 'package:mhi/features/patient/profile/presentation/widgets/patient_profile_details.dart';
 
 class PatientProfileView extends StatelessWidget {
@@ -31,19 +22,29 @@ class PatientProfileView extends StatelessWidget {
           ),
           verticleSpace(13),
           OptionButton(
-            icon: Icons.settings,
-            value: "الإعدادات",
-            onTap: () {},
-          ),
-          verticleSpace(13),
-          OptionButton(
             icon: Icons.person,
             value: "تعديل البيانات",
             onTap: () {},
           ),
           verticleSpace(13),
+          OptionButton(
+            icon: Icons.settings,
+            value: "الإعدادات",
+            onTap: () {
+              navigateToPatientSettingsView(context);
+            },
+          ),
+          verticleSpace(13),
         ],
       ),
+    ));
+  }
+
+  navigateToPatientSettingsView(BuildContext context) {
+    Navigator.of(context, rootNavigator: true).push(MaterialPageRoute(
+      builder: (context) {
+        return const PatientSettingsView();
+      },
     ));
   }
 }
