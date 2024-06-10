75279c99c888f58c92282c9324501c2e8cd49002
saber son <elmohandesstamera@gmail.com>
2024-06-10 08:33:21 +0300

    feat: providing  SavedDoctorsCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mhi/core/di/dependency_injection.dart';
 import 'package:mhi/features/auth/login/presentation/logic/cubit/login_cubit.dart';
 import 'package:mhi/features/auth/signup/presentation/logic/cubit/signup_cubit.dart';
+import 'package:mhi/features/patient/saved/presentation/Logic/cubit/saved_doctors_cubit.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/logic/cubit/specializes_cubit.dart';
 import 'package:mhi/features/patient/search/search_for_doctors.dart/presentation/logic/doctors/book_doctors_cubit.dart';
 import 'package:mhi/mhi_app.dart';
@@ -16,16 +16,11 @@ void main() async {
 
   runApp(MultiBlocProvider(
     providers: [
-      BlocProvider(
-        create: (context) => getIt<SignupCubit>(),
-      ),
-      BlocProvider(
-        create: (context) => getIt<LoginCubit>(),
-      ),
-      BlocProvider(
-        create: (context) => getIt<SpecializesCubit>(),
-      ),
-      BlocProvider(create: (context) => getIt<BookDoctorsCubit>())
+      BlocProvider(create: (context) => getIt<SignupCubit>()),
+      BlocProvider(create: (context) => getIt<LoginCubit>()),
+      BlocProvider(create: (context) => getIt<SpecializesCubit>()),
+      BlocProvider(create: (context) => getIt<BookDoctorsCubit>()),
+      BlocProvider(create: (context) => getIt<SavedDoctorsCubit>())
     ],
     child: const MhiApp(),
   ));
