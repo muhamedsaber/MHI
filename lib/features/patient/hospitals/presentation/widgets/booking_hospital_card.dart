e71c178431182189bbecb36c9df28b4fc07ace29
saber son <elmohandesstamera@gmail.com>
2024-06-16 03:36:11 +0300

    feat: Add hospital data sheet modal for booking hospital card

--- lib/features/patient/hospitals/presentation/widgets/booking_hospital_card.dart
+++ lib/features/patient/hospitals/presentation/widgets/booking_hospital_card.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mhi/core/common_ui/widgets/data_wide_shape.dart';
 import 'package:mhi/core/helper/app_textstyles.dart';
 import 'package:mhi/core/helper/extensions.dart';
 import 'package:mhi/core/helper/spacing.dart';
+import 'package:mhi/core/helper/theming.dart';
 import 'package:mhi/features/patient/hospitals/data/models/booking_hospital_model.dart';
+import 'package:mhi/features/patient/hospitals/presentation/widgets/hospital_data_sheet_body.dart';
 
 class BookingHospitalCard extends StatelessWidget {
-  const BookingHospitalCard({
-    super.key,
-    required this.hospitalModel,
-    required this.backgroundColor,
-    required this.textColor,
-    required this.onTap
-  });
+  const BookingHospitalCard(
+      {super.key,
+      required this.hospitalModel,
+      required this.backgroundColor,
+      required this.textColor,
+      required this.onTap});
 
   final BookingHospitalModel hospitalModel;
   final Color backgroundColor;
@@ -21,11 +24,17 @@ class BookingHospitalCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: context.width * 0.8,
-    
       child: GestureDetector(
-        onTap: onTap
-         
-        ,
+        onLongPress: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) {
+              return HospitalsDataSheetBody(hospitalModel: hospitalModel);
+            },
+          );
+        },
+        onTap: onTap,
         child: Card(
           color: backgroundColor,
           child: Padding(
@@ -35,18 +44,17 @@ class BookingHospitalCard extends StatelessWidget {
               children: [
                 Flexible(
                   child: Text(hospitalModel.name ?? "غير معروف",
-                      style: AppTextStyles.jannat18BoldWhite.copyWith(
-                          fontSize: 24.0,
-                          color:textColor)),
+                      style: AppTextStyles.jannat18BoldWhite
+                          .copyWith(fontSize: 24.0, color: textColor)),
                 ),
-                 verticleSpace(8),
-                Flexible(
+                verticleSpace(8),
+                Expanded(
                   child: Text(hospitalModel.address ?? "غير معروف",
-                      style: AppTextStyles.jannat18BoldWhite.copyWith(
-                          fontSize: 17.0,
-                          color:textColor)),
+                      textAlign: TextAlign.right,
+                      style: AppTextStyles.jannat18BoldWhite
+                          .copyWith(fontSize: 17.0, color: textColor)),
                 ),
-               verticleSpace(20)
+                verticleSpace(20)
               ],
             ),
           ),
---
+++ lib/features/patient/hospitals/presentation/widgets/hospital_data_sheet_body.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mhi/core/common_ui/widgets/data_wide_shape.dart';
+import 'package:mhi/core/helper/extensions.dart';
+import 'package:mhi/core/helper/spacing.dart';
+import 'package:mhi/core/helper/theming.dart';
+import 'package:mhi/features/patient/hospitals/data/models/booking_hospital_model.dart';
+
+class HospitalsDataSheetBody extends StatelessWidget {
+  const HospitalsDataSheetBody({super.key, required this.hospitalModel});
+  final BookingHospitalModel hospitalModel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      height: 400.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.r),
+          topRight: Radius.circular(20.r),
+        ),
+        color:context.theme.scaffoldBackgroundColor
+      ),
+      child: Column(
+        children: [
+          verticleSpace(10),
+          AppThemeing.dragHandle(context),
+          verticleSpace(10),
+          DataWideShape(
+              title: "اسم المستشفي", value: hospitalModel.name ?? "غير معروف"),
+          verticleSpace(10),
+          DataWideShape(
+              title: "العنوان", value: hospitalModel.address ?? "غير معروف"),
+          verticleSpace(10),
+          DataWideShape(
+              title: "كود المستشفي", value: hospitalModel.code ?? "غير معروف"),
+        ],
+      ),
+    );
+  }
+}
